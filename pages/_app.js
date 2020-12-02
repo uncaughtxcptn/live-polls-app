9d313b901f5d975a3d1e633b09e829a2fd003655
Arnelle Balane <arnellebalane@gmail.com>
2020-12-02 23:20:41 +0800

    Reset template and styles and load Google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:wght@400;700&display=swap"
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
